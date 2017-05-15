9e69f28985996176b63ca90cf23c2c04bac5f54a
喵叔catuncle <andych008@163.com>
2017-05-15 17:00:35 +0800

    把视图放在views下面

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
-import Todo from '@/components/Todo'
+import Hello from '@/views/Hello'
+import Todo from '@/views/Todo'
 
 Vue.use(Router)
 
